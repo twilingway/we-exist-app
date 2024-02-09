a127b3e0f8fb9392115268f1916f3a6fbae82368
ASLavrishchev <ASLavrishchev@beeline.ru>
2024-02-09 09:18:34 +0300

    add reload

--- src/features/ReloadPrompt/ReloadPrompt.tsx
+++ src/features/ReloadPrompt/ReloadPrompt.tsx
@@ -2,14 +2,20 @@ import { useRegisterSW } from 'virtual:pwa-register/react';
 
 import './ReloadPrompt.css'
 
+const UPDATE_CHECK_INTERVAL = 20000;
+
 function ReloadPrompt() {
     const {
         needRefresh: [needRefresh, setNeedRefresh],
         updateServiceWorker,
     } = useRegisterSW({
-        onRegistered(r) {
+        onRegisteredSW(swUrl,r ) {
             // eslint-disable-next-line prefer-template
-            console.log('SW Registered: ' + r)
+            console.log('SW Registered: ' + swUrl);
+            r && setInterval(async () => {
+                console.log('Refresh Checker: checking for update');
+                await r.update();
+            }, UPDATE_CHECK_INTERVAL);
         },
         onRegisterError(error) {
             console.log('SW registration error', error)
